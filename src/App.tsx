7538c15d52b4ce0a0a2bc7d28c005dc457695f63
kky8822 <99176588+kky8822@users.noreply.github.com>
2022-06-14 08:33:13 +0900

    220614_AnimatePresence

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import {
+  AnimatePresence,
   motion,
   useMotionValue,
   useTransform,
   useViewportScroll,
 } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Wrapper = styled(motion.div)`
   height: 100vh;
@@ -22,6 +23,11 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
+  position: absolute;
 `;
 
 const BiggerBox = styled.div`
@@ -35,9 +41,14 @@ const BiggerBox = styled.div`
 `;
 
 const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 180 },
-  tap: { scale: 1, borderRadius: "100px" },
-  drag: { backgroundColor: "rgb(0,0,0)", transition: { duration: 10 } },
+  entry: (back: boolean) => ({ x: back ? -500 : 500, opacity: 0, scale: 0 }),
+  center: { x: 0, opacity: 1, scale: 1, transitioin: { duration: 0.3 } },
+  exit: (back: boolean) => ({
+    x: back ? 500 : -500,
+    opacity: 0,
+    scale: 0,
+    transition: { duration: 0.3 },
+  }),
 };
 
 const Svg = styled.svg`
@@ -45,6 +56,11 @@ const Svg = styled.svg`
   height: 300px;
   stroke: white;
   stroke-width: 2;
+`;
+
+const Buttons = styled.button`
+  position: absolute;
+  bottom: 300px;
 `;
 
 const svgVariants = {
@@ -56,27 +72,35 @@ const svgVariants = {
 };
 
 function App() {
-  const x = useMotionValue(0);
-  const rotateZ = useTransform(x, [-800, 800], [-360, 360]);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
 
   return (
     <Wrapper>
-      <Svg
-        focusable="false"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-      >
-        <motion.path
-          variants={svgVariants}
-          initial={"start"}
-          animate={"end"}
-          transition={{
-            default: { duration: 5 },
-            fill: { duration: 2, delay: 5 },
-          }}
-          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
-        />
-      </Svg>
+      <AnimatePresence exitBeforeEnter custom={back}>
+        <Box
+          custom={back}
+          variants={boxVariants}
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}
+        >
+          {visible}
+        </Box>
+      </AnimatePresence>
+      <Buttons>
+        <button onClick={nextPlease}>next</button>
+        <button onClick={prevPlease}>prev</button>
+      </Buttons>
     </Wrapper>
   );
 }
